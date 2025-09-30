9499b1339a4eddfd57851270070bd6fb5c1104f1
AVC-24 <adityavikram2004.chaudhary@gmail.com>
2025-09-30 18:01:13 +0530

    Remove unused asset imports in ChatContainer component

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { assets } from '../assets/assets';
-import help_icon from '../assets/help.png'
-import avatar_icon from '../assets/avatar.png'
-import profile_alison from '../assets/profile_alison.avif';
 import { formatTimestamp } from '../lib/utils';
 import { ChatContext } from '../../context/ChatContext';
 import { AuthContext } from '../../context/AuthContext';
